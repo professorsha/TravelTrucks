cc3604180452bc2627f14a75a30c1a2e44ab1336
Iryna Mykhailovska <bagira.irina.m@gmail.com>
2024-09-29 17:53:53 +0300

    change style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,20 @@
 import { NavLink } from 'react-router-dom';
+import logo from '../../images/logo.svg';
 import css from './Header.module.css';
 const Header = () => {
   return (
     <header className={css.container}>
       <nav className={css.navigation}>
-        <NavLink className={css.logo} to="/">
-          <svg width="136" height="15">
-            <use href="/images/logo.svg" />
-          </svg>
+        <NavLink to="/">
+          <img className={css.logo} src={logo} alt="Logo" />
         </NavLink>
         <div className={css.menu}>
           <NavLink className={css.link} to="/">
             Home
           </NavLink>
-          <NavLink to="/catalog">Catalog</NavLink>
+          <NavLink className={css.link} to="/catalog">
+            Catalog
+          </NavLink>
         </div>
       </nav>
     </header>
